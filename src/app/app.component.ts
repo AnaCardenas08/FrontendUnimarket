a272df556f76d21d18d219f57d32cb94f1cb2205
Pablo Bernal <vinseth.2010@gmail.com>
2023-05-28 20:56:18 -0500

    actualizaciones index y login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Unimarket';
 
   constructor(private router: Router) {
     // Inyecta el servicio Router en el constructor del componente
